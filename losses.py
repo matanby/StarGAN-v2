38d4a311b4b77831cb8144ca70252d67849ef0b7
Matan Ben-Yosef <matan.benyosef@d-id.com>
2020-01-28 12:45:00 +0200

    losses: added R1 regularization and using BCEWithLogitsLoss for more numerically stable adversarial loss calculation.

--- losses.py
+++ losses.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import torch
 from torch import nn
 
@@ -9,27 +11,39 @@ class AdversarialLoss(nn.Module):
     def __init__(self, d: Discriminator):
         super().__init__()
         self._d = d
+        self._bce = nn.BCEWithLogitsLoss()
 
     def forward(self,
                 x_real: torch.Tensor,
                 y_real: torch.Tensor,
                 x_fake: torch.Tensor,
-                y_fake: torch.Tensor) -> torch.Tensor:
+                y_fake: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
 
         assert utils.is_valid_image_tensor(x_real)
         assert utils.is_valid_tensor(y_real, num_dims=2, batch_size=x_real.shape[0])
         assert utils.is_valid_image_tensor(x_fake)
         assert utils.is_valid_tensor(y_fake, num_dims=2, batch_size=x_fake.shape[0])
 
-        d_real = self._d(x_real, y_real)
-        d_fake = self._d(x_fake, y_fake)
+        x_real.requires_grad = True
+        logits_real = self._d(x_real, y_real)
+        logits_fake = self._d(x_fake, y_fake)
 
-        l_adv = (
-            torch.mean(torch.log(d_real), dim=0) +
-            torch.mean(torch.log(1 - d_fake), dim=0)
+        l_adv_d = (
+                torch.mean(self._bce(logits_real, torch.ones_like(logits_real)), dim=0) +
+                torch.mean(self._bce(logits_fake, torch.zeros_like(logits_fake)), dim=0)
         )
 
-        return l_adv
+        l_adv_g = torch.mean(self._bce(logits_fake, torch.ones_like(logits_fake)), dim=0)
+
+        # R1 regularization
+        grads_x_real = torch.autograd.grad(
+            outputs=logits_real.sum(),
+            inputs=x_real,
+            create_graph=True,
+        )[0]
+        r1_reg = 0.5 * grads_x_real.view(x_real.shape[0], -1).norm(2, dim=1).mean()
+
+        return l_adv_d, l_adv_g, r1_reg
 
 
 class StyleReconstructionLoss(nn.Module):
